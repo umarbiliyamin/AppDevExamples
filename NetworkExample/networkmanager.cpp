e6b22d84a6537503dffbc18ecbfbef152a8bff4f
Laszlo Blazovics <blazovics.laszlo@aut.bme.hu>
2017-11-10 09:06:22 +0100

    JSON Decoding

--- NetworkExample/networkmanager.cpp
+++ NetworkExample/networkmanager.cpp
@@ -2,6 +2,10 @@
 #include <QNetworkReply>
 #include <QNetworkRequest>
 
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+
 NetworkManager::NetworkManager(QObject *parent) : QObject(parent)
 {
     manager = new QNetworkAccessManager();
@@ -22,11 +26,61 @@ void NetworkManager::loadWebPage(){
 
 }
 
-
-
-void NetworkManager::myReplyFinished(QNetworkReply *reply)
+//
+void NetworkManager::myReplyFinished(QNetworkReply*reply)
 {
     QByteArray webData = reply->readAll();
-    QString webDataString = QString(webData);
-    qDebug()<<webDataString;
+
+    QList<QPair<QString,QString>> graphValues;
+
+    //QString webDataString = QString(webData);
+
+    QJsonDocument document = QJsonDocument::fromJson(webData);
+
+    if(document.isArray() == true){
+        //retreive the array
+        QJsonArray rootArray = document.array();
+    }
+
+    else if(document.isObject() == true)
+    {
+
+        QJsonObject rootObject = document.object();
+
+        QJsonObject timeSeries = rootObject["Time Series (Daily)"].toObject();
+        QStringList keys = timeSeries.keys();
+        for (QString k : keys){
+            QJsonObject dayValues = timeSeries[k].toObject();
+            QString openValue =  dayValues["1. open"].toString();
+
+            QPair<QString,QString> dataItem;
+            dataItem.first = k;
+            dataItem.second = openValue;
+
+            graphValues.append(dataItem);
+        }
+
+/*
+        QStringList keys = rootObject.keys();
+        for (QString k : keys){
+
+
+            if(rootObject[k].isObject() == true){
+                QJsonObject object = rootObject[k].toObject();
+
+                for (QString childkey : object.keys()){
+                    qDebug() << childkey;
+                }
+            }
+        }
+*/
+    }
+
+    for (int i=0; i<graphValues.size(); i++){
+        QPair<QString,QString> data = graphValues[i];
+        qDebug()<<data.first <<" - "<<data.second;
+    }
+
+
+    //qDebug()<<webDataString;
 }
